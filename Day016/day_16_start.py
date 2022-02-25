6f0c2ee730bd1b25c64382019668bf1f48baa37a
May Lin <40640630+maymlin@users.noreply.github.com>
2022-02-25 00:10:39 -0600

    Continue Day 16

--- Day016/day_16_start.py
+++ Day016/day_16_start.py
@@ -4,12 +4,16 @@ Day 16: Intermediate - Object Oriented Programming (OOP)
 https://docs.python.org/3/library/turtle.html
 """
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+import turtle
 
-timmy = Turtle()
+timmy = turtle.Turtle()
 timmy.shape("turtle")
-timmy.color("red", "green")
+timmy.color("green")
+print(timmy.position())
+timmy.forward(300)
+print(timmy.position())
 
-my_screen = Screen()
+my_screen = turtle.Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
