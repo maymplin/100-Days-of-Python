48382bc9a1cc1b0055d253084fdcf94fdfad1685
May Lin <40640630+maymlin@users.noreply.github.com>
2022-03-09 21:20:06 -0600

    Before refactoring to read score from data.txt

--- Day024/day-24/main.py
+++ Day024/day-24/main.py
@@ -1,7 +1,26 @@
-file = open("my_file.txt")
+# Video 220
+# Python documention - https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
-contents = file.read()
+# file = open("my_file.txt")
+#
+# contents = file.read()
+#
+# print(contents)
+#
+# file.close()
 
-print(contents)
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
 
-file.close()
+# # writes over existing file with "w" mode
+# with open("my_file.txt", mode="w") as file:
+#     file.write("New text.")
+
+# append
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nHello there.")
+
+# if a file doesn't yet exist when trying to write to a file, Python will create the file automatically
+with open("new_file.txt", mode="w") as file:
+    file.write("A brand new file.")
