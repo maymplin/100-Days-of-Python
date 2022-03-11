46ed200609c6fe2be4dc5ff531d3812e9bfb4345
May Lin <40640630+maymlin@users.noreply.github.com>
2022-03-10 23:08:19 -0600

    Refactor Mail Merge Project

--- Day024/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ Day024/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -7,25 +7,36 @@
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
+PLACEHOLDER = "[name]"
+
 names = []
 
+# with open("Input/Names/invited_names.txt", "r") as file:
+#     names = [name.replace("\n", "") for name in file.readlines()]
+#     print(names)
+
+# for name in names:
+#     fin = open("Input/Letters/starting_letter.txt")
+#     out_file_path = f"Output/ReadyToSend/letter_for_{name}.txt"
+#     fout = open(out_file_path, "wt")
+#
+#     for line in fin:
+#         if "[name]" in line:
+#             fout.write(line.replace(PLACEHOLDER, name))
+#         else:
+#             fout.write(line)
+#
+#     fin.close()
+#     fout.close()
+
 with open("Input/Names/invited_names.txt", "r") as file:
-    names = [name.replace("\n", "") for name in file.readlines()]
-    print(names)
+    names = [name.strip() for name in file.readlines()]
 
-for name in names:
-    fin = open("Input/Letters/starting_letter.txt")
-    out_file_path = f"Output/ReadyToSend/letter_for_{name}.txt"
-    fout = open(out_file_path, "wt")
+with open("Input/Letters/starting_letter.txt") as start_file:
+    start_letter = start_file.read()
 
-    for line in fin:
-        if "[name]" in line:
-            fout.write(line.replace("[name]", name))
-        else:
-            fout.write(line)
-
-    fin.close()
-    fout.close()
-
-
+    for name in names:
+        new_letter = start_letter.replace(PLACEHOLDER, name)
+        with open(f"Output/ReadyToSend/letter_for_{name}", "w") as completed_letter:
+            completed_letter.write(new_letter)
 
