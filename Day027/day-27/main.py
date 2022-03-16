f5c80b6172a2bb019aa2824faaf59cf989da5df5
May Lin <40640630+maymlin@users.noreply.github.com>
2022-03-15 22:52:32 -0500

    Day 27

--- Day027/day-27/main.py
+++ Day027/day-27/main.py
@@ -11,6 +11,23 @@ window.minsize(500, 300)
 my_label = tkinter.Label(text="I am a Label", font=("Ariel", 24, "bold"))
 my_label.pack()
 
+my_label["text"] = "New Text"           # this and the next line my_label.config() do the same thing
+my_label.config(text="Another text")
+
+
+# Button
+def button_clicked():
+    # print("I got clicked")
+    my_label["text"] = user_input.get()
+
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+user_input = tkinter.Entry(width=10)
+user_input.pack()
+
 
 window.mainloop()
 
