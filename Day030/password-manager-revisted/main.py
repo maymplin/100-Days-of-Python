c176ec7b23befe0b882607f293cc40e5a4389c21
May Lin <40640630+maymlin@users.noreply.github.com>
2022-03-23 23:44:30 -0500

    Add URL to Day 30 Password Manager App Revisited

--- Day030/password-manager-revisted/main.py
+++ Day030/password-manager-revisted/main.py
@@ -33,7 +33,8 @@ def generate_password():
 
 def display_confirmation():
     if len(username_entry.get()) > 0 and len(password_entry.get()) > 0:
-        is_ok = messagebox.askokcancel(title=website_entry, message=f"Username: {username_entry.get()}\n"
+        is_ok = messagebox.askokcancel(title=website_entry, message=f"Website: {website_entry.get()}\n"
+                                                                    f"Username: {username_entry.get()}\n"
                                                                     f"Password: {password_entry.get()}\n"
                                                                     f"Save?")
         if is_ok:
@@ -47,9 +48,10 @@ def display_confirmation():
 def save_password_to_file():
     with open("password.txt", "a") as file:
         website = website_entry.get()
+        url = url_entry.get()
         username = username_entry.get()
         password = password_entry.get()
-        file.write(f"{website} | {username} | {password}\n")
+        file.write(f"{website} | {url} | {username} | {password}\n")
 
 
 # https://tkdocs.com/tutorial/widgets.html#entry
@@ -78,11 +80,14 @@ canvas.grid(column=1, row=0)
 website_label = tkinter.Label(text="Website:", bg="white", font=FONT)
 website_label.grid(column=0, row=1)
 
+url_label = tkinter.Label(text="URL:", bg="white", font=FONT)
+url_label.grid(column=0, row=2)
+
 username_label = tkinter.Label(text="Email/Username:", bg="white", font=FONT)
-username_label.grid(column=0, row=2)
+username_label.grid(column=0, row=3)
 
 password_label = tkinter.Label(text="Password:", bg="white", font=FONT)
-password_label.grid(column=0, row=3)
+password_label.grid(column=0, row=4)
 
 # Entries
 # https://tkdocs.com/tutorial/widgets.html#entry
@@ -90,20 +95,23 @@ website_entry = tkinter.Entry(width=35)
 website_entry.focus()
 website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
 
+url_entry = tkinter.Entry(width=35)
+url_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
+
 username_entry = tkinter.Entry(width=35)
 username_entry.insert(0, "me@gmail.com")  # pre-populate field
-username_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
+username_entry.grid(column=1, row=3, columnspan=2, sticky="EW")
 
 password_entry = tkinter.Entry()
-password_entry.grid(column=1, row=3, sticky="EW")
+password_entry.grid(column=1, row=4, sticky="EW")
 
 # Buttons
 generate_password_button = tkinter.Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(column=2, row=3, sticky="EW")
+generate_password_button.grid(column=2, row=4, sticky="EW")
 
 add_button = tkinter.Button(text="Add", width=35)
 add_button.config(command=add_button_clicked)
-add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
+add_button.grid(column=1, row=5, columnspan=2, sticky="EW")
 
 
 window.mainloop()
