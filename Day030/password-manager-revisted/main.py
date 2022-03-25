a531ac500ee11543abd52e489c578364384e1264
May Lin <40640630+maymlin@users.noreply.github.com>
2022-03-24 23:11:22 -0500

    Complete Day 30 - Intermediate - Errors, Exceptions and JSON Data: Imrpoving the Password

--- Day030/password-manager-revisted/main.py
+++ Day030/password-manager-revisted/main.py
@@ -31,14 +31,18 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
-
-def display_confirmation():
-    if len(username_entry.get()) > 0 and len(password_entry.get()) > 0:
-        save_password_to_file()
-        clear_entries()
+def confirm_entries():
+    if len(website_entry.get()) > 0 and len(username_entry.get()) > 0 and len(password_entry.get()) > 0:
+        return True
     else:
-        messagebox.showinfo(title="Oops", message="Website and username/email must not be empty.")
-    
+        messagebox.showinfo(title="Oops", message="Website, username/email and password must not be empty.")
+    return False
+
+
+def write_to_json(data, file):
+    with open(file, "w") as data_file:
+        json.dump(data, data_file, indent=4)
+
 
 # https://www.geeksforgeeks.org/python-append-to-a-file/
 def save_password_to_file():
@@ -54,10 +58,18 @@ def save_password_to_file():
         }
     }
 
-    with open("data.json", "w") as data_file:
-        json.dump(new_data, data_file, indent=4)
-
-        # file.write(f"{website} | {url} | {username} | {password}\n")
+    try:
+        with open("data.json", "r") as data_file:
+            # Read existing data
+            data = json.load(data_file)
+    except FileNotFoundError:
+        # create file if doesn't exist already and add data
+        write_to_json(new_data, "data.json")
+    else:
+        # Update old data with new data
+        data.update(new_data)
+        # Save updated data
+        write_to_json(data, "data.json")
 
 
 # https://tkdocs.com/tutorial/widgets.html#entry
@@ -65,10 +77,39 @@ def clear_entries():
     website_entry.delete(0, tkinter.END)
     website_entry.focus()
     password_entry.delete(0, tkinter.END)
+    url_entry.delete(0, tkinter.END)
 
 
 def add_button_clicked():
-    display_confirmation()
+    if confirm_entries():
+        save_password_to_file()
+        clear_entries()
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+def display_search_not_found():
+    messagebox.showinfo(title="Oops", message=f"No details for {website_entry.get()} exists.")
+
+
+def search_password():
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        display_search_not_found()
+    else:
+        search_term = website_entry.get()
+        website = data.get(search_term)
+        if website is not None:
+            username = website.get("username")
+            password = website.get("password")
+            messagebox.showinfo(title=search_term, message=f"username: {username}\n"
+                                                           f"password: {password}")
+            window.clipboard_clear()
+            window.clipboard_append(password)
+        else:
+            display_search_not_found()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -99,7 +140,7 @@ password_label.grid(column=0, row=4)
 # https://tkdocs.com/tutorial/widgets.html#entry
 website_entry = tkinter.Entry(width=35)
 website_entry.focus()
-website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
+website_entry.grid(column=1, row=1)
 
 url_entry = tkinter.Entry(width=35)
 url_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
@@ -112,6 +153,9 @@ password_entry = tkinter.Entry()
 password_entry.grid(column=1, row=4, sticky="EW")
 
 # Buttons
+search_button = tkinter.Button(text="Search", command=search_password)
+search_button.grid(column=2, row=1, sticky="EW")
+
 generate_password_button = tkinter.Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(column=2, row=4, sticky="EW")
 
