00be209172dd6d2d6702f9cbd8508d2dd8e4b8bf
May Lin <40640630+maymlin@users.noreply.github.com>
2022-03-28 15:34:17 -0500

    Complete Day 31 - Intermediate - Flash Card App Capstone Project

---
+++ Day028/pomodoro-start/pomodoro45.pyw
@@ -0,0 +1,115 @@
+# https://colorhunt.co/
+# https://www.youtube.com/watch?v=UZX5kH72Yx4
+import tkinter
+import math
+
+
+def focus_window(option):
+    if option == "on":
+        window.deiconify()
+        window.focus_force()
+        window.attributes('-topmost', 1)
+    elif option == "off":
+        window.attributes('-topmost', 0)
+
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 45
+SHORT_BREAK_MIN = 10
+LONG_BREAK_MIN = 15
+
+reps = 0
+timer = None
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+
+def reset_timer():
+    window.after_cancel(timer)
+    timer_label.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text="25:00")
+    check_label.config(text="")
+
+    global reps
+    reps = 0
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+def start_timer():
+    global reps
+
+    reps += 1
+
+    work_sec = WORK_MIN*60
+    short_break_sec = SHORT_BREAK_MIN*60
+    long_break_sec = LONG_BREAK_MIN*60
+
+    # window.attributes('-topmost', 1)
+
+    if reps % 2 == 1:
+        timer_label.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+        focus_window("off")
+    elif reps == 8:
+        timer_label.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+        focus_window("on")
+        reps = 0
+    else:
+        timer_label.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+        focus_window("on")
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+
+def count_down(count):
+    count_min = math.floor(count/60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f'{count_min:02}:{count_sec:02}')
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        window.bell()
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_label.config(text=marks)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = tkinter.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = tkinter.PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="25:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+timer_label = tkinter.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
+timer_label.grid(column=1, row=0)
+
+start_button = tkinter.Button(text="Start", command=start_timer)
+start_button.grid(column=0, row=2)
+
+rest_button = tkinter.Button(text="Reset", highlightthickness=0, command=reset_timer)
+rest_button.grid(column=2, row=2)
+
+check_label = tkinter.Label(text="", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 17, "bold"), highlightthickness=0)
+check_label.grid(column=1, row=3)
+
+window.mainloop()
--- Day031/flash-card-project-start/main.py
+++ Day031/flash-card-project-start/main.py
@@ -9,6 +9,9 @@
 import tkinter
 import pandas as pd
 import random
+from os.path import exists
+from os import remove
+from tkinter import messagebox
 
 # --------------------------- CONSTANTS ------------------------------ #
 
@@ -18,44 +21,82 @@ WORD_FONT = ("Ariel", 60, "bold")
 
 # ------------------------- GLOBAL VARIABLES -------------------------- #
 
-next_word = ""
+new_word = ""
 timer = None
+fr_eng_dict = None
 
 
 # --------------------------- FLIP CARD ------------------------------- #
 
 def find_english():
-    return fr_eng_dict[next_word]
+    return new_word["English"]
 
 
 def display_back_of_card():
-    english_word = find_english()
     canvas.itemconfig(title_text, text="English", fill="white")
-    canvas.itemconfig(word_text, text=english_word, fill="white")
+    canvas.itemconfig(word_text, text=find_english(), fill="white")
     canvas.itemconfig(canvas_img, image=back_img)
     window.after_cancel(timer)
 
 
-# ------------------------- GENERATE WORDS ---------------------------- #
+# ----------------------- TRACK UNKNOWN WORDS ------------------------ #
 
-# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_dict.html
-data = pd.read_csv("data/french_words.csv")
-french_words = data.French.tolist()
-english_words = data.English.tolist()
-fr_eng_dict = dict(zip(french_words, english_words))
+def play_again():
+    return tkinter.messagebox.askyesno("Play Again?", f"You know all the words already. Good job!\n"
+                                                      f"Do you want to practice from the beginning again?")
+
+
+def reset_word_dict():
+    global fr_eng_dict
+    fr_eng_dict = pd.read_csv("data/french_words.csv").to_dict(orient="record")
+
+
+if exists("unknown_words.csv"):
+    df = pd.read_csv("unknown_words.csv")
+    fr_eng_dict = df.to_dict(orient="record")
+else:
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_dict.html
+    reset_word_dict()
+
+
+def update_unknown_words_file():
+    updated_df = pd.DataFrame(fr_eng_dict)
+    updated_df.to_csv("unknown_words.csv", index=False)
+
+
+def remove_known_word():
+    window.after_cancel(timer)
+    fr_eng_dict.remove(new_word)
+    update_unknown_words_file()
+    display_new_word()
+
+# ------------------------- GENERATE WORDS ---------------------------- #
 
 
 def pick_random_word():
-    return random.choice(french_words)
+    if len(fr_eng_dict) == 0:
+        if play_again():
+            reset_word_dict()
+            return random.choice(fr_eng_dict)
+        else:
+            canvas.delete("all")
+            window.quit()
+            remove("unknown_words.csv")
+    else:
+        return random.choice(fr_eng_dict)
 
 
 def display_new_word():
-    global next_word, timer
-    next_word = pick_random_word()
+    global new_word, timer
+    new_word = pick_random_word()
     canvas.itemconfig(title_text, text="French", fill="black")
-    canvas.itemconfig(word_text, text=next_word, fill="black")
-    canvas.itemconfig(canvas_img, image=front_img)
-    timer = window.after(3000, display_back_of_card)
+    try:
+        canvas.itemconfig(word_text, text=new_word["French"], fill="black")
+    except TypeError:
+        print("To play, you must choose to start game again.")
+    else:
+        canvas.itemconfig(canvas_img, image=front_img)
+        timer = window.after(3000, display_back_of_card)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -64,8 +105,10 @@ window = tkinter.Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR, highlightthickness=0)
 
+
 # canvas
 canvas = tkinter.Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+
 front_img = tkinter.PhotoImage(file="images/card_front.png")
 back_img = tkinter.PhotoImage(file="images/card_back.png")
 canvas_img = canvas.create_image(400, 263, image=front_img)
@@ -79,7 +122,7 @@ wrong_button = tkinter.Button(image=wrong_img, highlightthickness=0, command=dis
 wrong_button.grid(row=1, column=0)
 
 right_img = tkinter.PhotoImage(file="images/right.png")
-right_button = tkinter.Button(image=right_img, highlightthickness=0, command=display_new_word)
+right_button = tkinter.Button(image=right_img, highlightthickness=0, command=remove_known_word)
 right_button.grid(row=1, column=1)
 
 display_new_word()
